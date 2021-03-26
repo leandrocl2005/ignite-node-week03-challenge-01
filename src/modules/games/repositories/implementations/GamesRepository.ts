fdb10f20f3e13eb6e5df4d89d7e3ff91a5174a35
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-03-26 11:20:40 -0300

    first try

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -27,24 +27,11 @@ export class GamesRepository implements IGamesRepository {
     /* return this.repository
       .createQueryBuilder() */
 
-    const users = await this.repository
+    return this.repository
       .createQueryBuilder("game")
       .leftJoinAndSelect("game.users", "user")
-      .where("game.id = :id", { id })
-      .getMany();
-
-    console.log("###", users);
-
-    return [
-      {
-        id: "1",
-        first_name: "bob",
-        last_name: "bia",
-        email: "",
-        games: [],
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ];
+      .where("game.id =  :id", { id })
+      .select(["email", "first_name", "last_name"])
+      .execute();
   }
 }
