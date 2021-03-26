00ebb718d02c201f6f6a26c1f80c94517061754a
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-03-26 10:32:09 -0300

    falta 1 teste

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Como rodar
+
+- > git clone
+- > yarn
+- Criar database `queries_challenge`
+- Configura _ormconfig.json_
+- > yarn typeorm migration:run
+- > yarn test
+
+## Testes faltantes
+
+Função _findUsersByGameId_ do arquivo _src/modules/games/repositories/implementations/GamesRespository.ts_
--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import { User } from "../../../users/entities/User";
+import { Game } from "../../entities/Game";
 
-import { IGamesRepository } from '../IGamesRepository';
+import { IGamesRepository } from "../IGamesRepository";
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -14,17 +14,37 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .where(`games.title ILIKE '%${param}%'`)
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM games`); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    /* return this.repository
+      .createQueryBuilder() */
+
+    const users = await this.repository
+      .createQueryBuilder("game")
+      .leftJoinAndSelect("game.users", "user")
+      .where("game.id = :id", { id })
+      .getMany();
+
+    console.log("###", users);
+
+    return [
+      {
+        id: "1",
+        first_name: "bob",
+        last_name: "bia",
+        email: "",
+        games: [],
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ];
   }
 }
